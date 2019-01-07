0b1d93283d6f80fb41feeb76e2127e3c851063f2
Sheikh Sanjid <46426568+hunterzolemon@users.noreply.github.com>
2019-01-08 05:04:17 +0600

    Create get_host_name.php

---
+++ get_host_name.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Registration Form</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+
+<?php
+   $hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+   
+   echo $hostname;
+?>
+</body>
+</html>
