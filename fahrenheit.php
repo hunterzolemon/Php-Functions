ab053951373271e9035345d795971e8c7eac1f93
Sheikh Sanjid <46426568+hunterzolemon@users.noreply.github.com>
2019-01-08 01:42:56 +0600

    Create fahrenheit.php

---
+++ fahrenheit.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+function FTC($c) {
+    
+    return $c * 9/5 + 32;
+}
+
+echo FTC(100);
+echo "\n";
+echo FTC(0);
+echo "\n";
+echo FTC(30);
+echo "\n";
+
+?>
+</body>
+</html>
