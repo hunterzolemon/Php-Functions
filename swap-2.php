90928ca2feac38283d3b268b341f649bb6cb6061
Sheikh Sanjid <46426568+hunterzolemon@users.noreply.github.com>
2019-01-08 01:47:47 +0600

    Create swap-2.php

---
+++ swap-2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+function swap(&$a, &$b) {
+
+    $temp = $a;
+    $a = $b;
+    $b = $temp;
+    echo "Inside the swap function:\n";
+    echo "\$a is $a \n";
+    echo "\$b is $b \n";
+}
+
+$a = 4;
+$b = 7;
+
+echo "Outside the swap function:\n";
+echo "\$a is $a \n";
+echo "\$b is $b \n";
+
+swap($a, $b);
+
+echo "Outside the swap function:\n";
+echo "\$a is $a \n";
+echo "\$b is $b \n";
+
+?>
+</body>
+</html>
