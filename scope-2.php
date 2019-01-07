9e3cd04c62c54e3a82170047c27eea01f85821ad
Sheikh Sanjid <46426568+hunterzolemon@users.noreply.github.com>
2019-01-08 01:49:24 +0600

    Create scope-2.php

---
+++ scope-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+$value = 4;
+
+function simple() {
+    $value = 3;
+    echo $value, "\n";
+}
+
+simple();
+
+echo $value, "\n";
+
+?>
+</body>
+</html>
