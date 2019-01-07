aa85f25c771e815138468c2cee2f98b6b4c893e1
Sheikh Sanjid <46426568+hunterzolemon@users.noreply.github.com>
2019-01-08 01:46:28 +0600

    Create function_method-2.php

---
+++ function_method-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+$var = function() {
+    
+    echo "This is anonymous function\n";
+};
+
+$var();
+
+?>
+</body>
+</html>
