dc0c3d2b24f1b4fa0d86a91e78645983c6cee56c
Sheikh Sanjid <46426568+hunterzolemon@users.noreply.github.com>
2019-01-08 08:39:06 +0600

    Create get_class.php

---
+++ get_class.php
@@ -0,0 +1,19 @@
+<?php
+
+class foo {
+    function name()
+    {
+        echo "My name is " , get_class($this) , "\n";
+    }
+}
+
+// create an object
+$bar = new foo();
+
+// external call
+echo "Its name is ",get_class($bar) , "\n";
+
+// internal call
+$bar->name();
+
+?>
