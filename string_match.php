eba72fffe0af0dea083242a001562c6b05b987ca
Sheikh Sanjid <46426568+hunterzolemon@users.noreply.github.com>
2019-01-08 07:41:00 +0600

    Create string_match.php

---
+++ string_match.php
@@ -0,0 +1,21 @@
+<?php
+
+echo strcmp("Cloudways","CLOUDWAYS");
+
+echo "<br>";
+
+echo strcmp("cloudways","cloudways");//Both the strings are equal
+
+echo "<br>";
+
+echo strcmp("Cloudways","Hosting");
+
+echo "<br>";
+
+echo strcmp("a","b");//compares alphabetically
+
+echo "<br>";
+
+echo strcmp("abb baa","abb baa caa");//compares both strings and returns the result in terms of number of characters.
+
+?>
