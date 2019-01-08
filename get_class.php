a5811803a2cfac9d979e7d4ea873da8f838f588f
Sheikh Sanjid <46426568+hunterzolemon@users.noreply.github.com>
2019-01-08 08:58:36 +0600

    Update get_class.php

--- get_class.php
+++ get_class.php
@@ -17,3 +17,25 @@ echo "Its name is ",get_class($bar) , "\n";
 $bar->name();
 
 ?>
+//num 2 example//
+<?php
+
+class foo {
+    function name()
+    {
+        echo "main " , get_class($this) , "\n";
+
+    }
+    function name2()
+    {
+        echo "main2 " , get_class($this) , "\n";
+    }
+}
+
+
+$bar = new foo();
+                        echo $bar->name();
+                        echo $bar->name2();
+
+
+?>
