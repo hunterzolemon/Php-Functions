9aa27d4b61b33ba78a5a5865ad34fe40cf50dc0b
Sheikh Sanjid <46426568+hunterzolemon@users.noreply.github.com>
2019-03-29 09:34:51 +0600

    Add files via upload

---
+++ serverFunction.php
@@ -0,0 +1,19 @@
+
+<html>
+    <body>
+
+
+           <?php
+           echo $_SERVER['SERVER_NAME'];echo "<br>";
+           echo $_SERVER['DOCUMENT_ROOT'];      echo "<br>";
+           echo $_SERVER['HTTP_HOST'];      echo "<br>";
+           echo $_SERVER['REMOTE_ADDR'];echo "<br>";
+           echo $_SERVER['SCRIPT_FILENAME'];echo "<br>";
+           echo $_SERVER['SERVER_ADMIN'];echo " ";
+           echo $_SERVER['SERVER_PORT'];echo "<br>";
+           echo $_SERVER['HTTPS'];      echo "<br>";
+           echo $_SERVER['REQUEST_URI'];
+           ?>
+
+    </body>
+</html>
